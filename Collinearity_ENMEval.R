2bb237a191a5ab1401cbe327d748d80578e9dd9c
Courtney Stuart <courtney.e.stuart@gmail.com>
2021-05-13 13:19:38 -0600

    Collinearity assessment & MaxEnt settings

--- Collinearity_ENMEval.R
+++ Collinearity_ENMEval.R
@@ -4,35 +4,37 @@ libraries("raster", "sf", "tmap", "dplyr", "usdm", "rgdal", "usdm",
           "sdmpredictors", "PNWColors", "corrplot", "Cairo")
 
 # working directories
-setwd("Z:/Courtney/Stuart_MSc_Ch1/")
-rasterOptions(tmpdir = "Z:/Courtney/Stuart_MSc_Ch1/Temp/") # temporary working directory for raster package
-csv_wd = "Z:/Courtney/Stuart_MSc_Ch1/GitHub/FL_Habitat_Suitability/Data/" # for writing out tabular data
-train_wd = "Z:/Courtney/Stuart_MSc_Ch1/Modeling_Data/Training/" # for writing out training data
-test_wd = "Z:/Courtney/Stuart_MSc_Ch1/Modeling_Data/Testing/" # for writing out testing data
+setwd("Z:/Courtney/Stuart_MSc_Ch1/") # main project folder
+
+# data directories 
+temp_wd = "Z:/Courtney/Stuart_MSc_Ch1/Temporary/" # temporary files
+csv_wd = "Z:/Courtney/Stuart_MSc_Ch1/GitHub/FL_Habitat_Suitability/Data/" # for writing tabular data
+fish_wd = "Z:/Courtney/Stuart_MSc_Ch1/Species_Occurrence/" # for fish data
+spatial_wd = "Z:/Courtney/Stuart_MSc_Ch1/Spatial_Predictors/" # for spatial predictor rasters
+gis_wd = "Z:/Courtney/Stuart_MSc_Ch1/GIS_Files/" # for any GIS shapefiles
 plots_wd = "Z:/Courtney/Stuart_MSc_Ch1/GitHub/FL_Habitat_Suitability/Figures/"
-temp_wd = "Z:/Courtney/Stuart_MSc_Ch1/Temp/"
 
 # save PROJ.4 string for standard projection (ESPG:26958 NAD 83/Florida East) and source LAT/LON data (EPSG:4326 WGS 84/World Geodetic System 1984)
 my_crs = CRS("+init=epsg:26958")
 gcs = CRS("+init=epsg:4326")
 
-# load environmental rasters from training area
-habitat = raster(paste0(train_wd, "Environmental/Habitat.asc"))
-mg_dist = raster(paste0(train_wd, "Environmental/Mangrove_Dist.asc"))
-depth = raster(paste0(train_wd, "Environmental/Depth.asc"))
-sd_depth = raster(paste0(train_wd, "Environmental/SD_Depth.asc"))
-slope = raster(paste0(train_wd, "Environmental/Slope.asc"))
-curvature = raster(paste0(train_wd, "Environmental/Curvature.asc"))
-plan_curve = raster(paste0(train_wd, "Environmental/Plan_Curve.asc"))
-bpi_fine = raster(paste0(train_wd, "Environmental/BPI_Fine.asc"))
-bpi_broad = raster(paste0(train_wd, "Environmental/BPI_Broad.asc"))
-rugosity = raster(paste0(train_wd, "Environmental/Rugosity.asc"))
-sum_temp = raster(paste0(train_wd, "Environmental/Mean_Sum_Temp.asc"))
-sum_do = raster(paste0(train_wd, "Environmental/Mean_Sum_DO.asc"))
-sum_sal = raster(paste0(train_wd, "Environmental/Mean_Sum_Sal.asc"))
-win_temp = raster(paste0(train_wd, "Environmental/Mean_Win_Temp.asc"))
-win_do = raster(paste0(train_wd, "Environmental/Mean_Win_DO.asc"))
-win_sal = raster(paste0(train_wd, "Environmental/Mean_Win_Sal.asc"))
+# load environmental rasters 
+habitat = raster(paste0(spatial_wd, "Habitat.asc"))
+mg_dist = raster(paste0(spatial_wd, "Mangrove_Dist.asc"))
+depth = raster(paste0(spatial_wd, "Depth.asc"))
+sd_depth = raster(paste0(spatial_wd, "Depth_St_Dev.asc"))
+slope = raster(paste0(spatial_wd, "Slope.asc"))
+curvature = raster(paste0(spatial_wd, "Curvature.asc"))
+plan_curve = raster(paste0(spatial_wd, "Plan_Curve.asc"))
+bpi_fine = raster(paste0(spatial_wd, "BPI_Fine.asc"))
+bpi_broad = raster(paste0(spatial_wd, "BPI_Broad.asc"))
+rugosity = raster(paste0(spatial_wd, "Rugosity.asc"))
+sum_temp = raster(paste0(spatial_wd, "Mean_Sum_Temp.asc"))
+sum_do = raster(paste0(spatial_wd, "Mean_Sum_DO.asc"))
+sum_sal = raster(paste0(spatial_wd, "Mean_Sum_Sal.asc"))
+win_temp = raster(paste0(spatial_wd, "Mean_Win_Temp.asc"))
+win_do = raster(paste0(spatial_wd, "Mean_Win_DO.asc"))
+win_sal = raster(paste0(spatial_wd, "Mean_Win_Sal.asc"))
 
 # define crs
 crs(habitat) = my_crs
@@ -52,26 +54,33 @@ crs(win_temp) = my_crs
 crs(win_do) = my_crs
 crs(win_sal) = my_crs
 
-# create raster stack for training area
+# create raster stack 
 env = stack(x = c(habitat, mg_dist, depth, sd_depth, slope, curvature, 
                   plan_curve, bpi_fine, bpi_broad, rugosity, sum_temp, sum_do, 
                   sum_sal, win_temp, win_do, win_sal))
 
 #### CORRELATION AND VIF ####
 set.seed(42) 
+cl = snow::makeCluster(10)
 
-# full pearson correlation matrix on all spatial predictors from the training area
+# full pearson correlation matrix on all spatial predictors
 pearson = pearson_correlation_matrix(env)
 head(pearson)
+pearson_rename = as.data.frame(pearson) %>%
+  rename(Slope = slope, BPI_Fine = bpi_fine, Rugosity = rugosity) 
+rownames(pearson_rename)[rownames(pearson_rename) == "bpi_fine"] = "BPI_Fine"
+rownames(pearson_rename)[rownames(pearson_rename) == "slope"] = "Slope"
+rownames(pearson_rename)[rownames(pearson_rename) == "rugosity"] = "Rugosity"
+pearson_rename = as.matrix(pearson_rename)
 
 # plot full correlation matrix
 palette = pnw_palette("Shuksan2", 200, type = "continuous")
 par(mar=c(0,0,0,0))
-corrplot(pearson, method = "color", col = palette, type = "upper",
+corrplot(pearson_rename, method = "color", col = palette, type = "upper",
          order = "original", addCoef.col = "black", number.cex = 0.55, 
          number.digits = 2, tl.col = "black", tl.srt = 40, tl.cex = 0.8)
 # save plot as png
-Cairo(file=paste0(plots_wd, "Full_Pearson_Correlation_Matrix.png"), 
+Cairo(file=paste0(plots_wd, "Correlation_Full_Predictor_Set.png"), 
       bg="white",
       type="png",
       units="in", 
@@ -80,38 +89,53 @@ Cairo(file=paste0(plots_wd, "Full_Pearson_Correlation_Matrix.png"),
       pointsize=12, 
       dpi=600)
 par(mar=c(0,0,0,0))
-corrplot(pearson, method = "color", col = palette, type = "upper",
+corrplot(pearson_rename, method = "color", col = palette, type = "upper",
          order = "original", addCoef.col = "black", number.cex = 0.55, 
          number.digits = 2, tl.col = "black", tl.srt = 40, tl.cex = 0.8)
 dev.off()
 
 # save as CSV
-write.csv(pearson, paste0(csv_wd, "Full_Pearson_Correlation_Matrix.csv"))
+write.csv(pearson_rename, paste0(csv_wd, "Correlation_Full_Predictor_Set.csv"))
 
 ## examine correlation coefficients and variance inflation factors (VIF)
 #install.packages('usdm')
 library(usdm)
-
-vif = vif(env, maxobservations = 10000)
+cl = snow::makeCluster(10)
+x = sampleRandom(env, 10000, na.rm = TRUE, xy = TRUE)
+snow::stopCluster(cl)
+x = as.data.frame(x) %>%
+  rename(Rugosity = rugosity,
+         BPI_Fine = bpi_fine,
+         Slope = slope) %>%
+  select(-x, -y)
+vif = vif(x)
 vif
-vifcor = vifcor(env, th = abs(0.7), maxobservations = 10000) 
-vifcor
-write.csv(vif, paste0(csv_wd, "Full_VIF.csv"), row.names = F)
+write.csv(vif, paste0(csv_wd, "VIF_Full_Predictor_Set.csv"), row.names = F)
 
-# reduce predictors that exceeded correlation and VIF thresholds
-# removing DO for both seasons, SD depth, plan curvature, winter temps
+# remove predictors that exceeded thresholds
+# removing DO for both seasons, SD depth, and plan curvature
 env2 = stack(x = c(habitat, mg_dist, depth, slope, curvature, bpi_fine, 
-                   bpi_broad, rugosity, sum_temp, sum_sal, win_sal))
+                   bpi_broad, rugosity, sum_temp, sum_sal, win_temp, win_sal))
 
 # reduced correlation matrix
+cl = snow::makeCluster(10)
 pearson2 = (pearson_correlation_matrix(env2))
 head(pearson2)
+snow::stopCluster(cl)
+
+# some variables are showing up as lower case, fix that
+pearson2_rename = as.data.frame(pearson2) %>%
+  rename(Slope = slope, BPI_Fine = bpi_fine, Rugosity = rugosity) 
+rownames(pearson2_rename)[rownames(pearson2_rename) == "bpi_fine"] = "BPI_Fine"
+rownames(pearson2_rename)[rownames(pearson2_rename) == "slope"] = "Slope"
+rownames(pearson2_rename)[rownames(pearson2_rename) == "rugosity"] = "Rugosity"
+pearson2_rename = as.matrix(pearson2_rename)
 
 # plot and save the reduced correlation matrix
-corrplot(pearson2, method = "color", col = palette, type = "upper",
+corrplot(pearson2_rename, method = "color", col = palette, type = "upper",
          order = "original", addCoef.col = "black", number.cex = 0.55, 
          number.digits = 2, tl.col = "black", tl.srt = 40, tl.cex = 0.8)
-Cairo(file=paste0(plots_wd, "Reduced_Pearson_Correlation_Matrix.png"), 
+Cairo(file=paste0(plots_wd, "Correlation_Reduced_Predictor_Set.png"), 
       bg="white",
       type="png",
       units="in", 
@@ -120,20 +144,26 @@ Cairo(file=paste0(plots_wd, "Reduced_Pearson_Correlation_Matrix.png"),
       pointsize=12, 
       dpi=300)
 par(mar=c(0,0,0,0))
-corrplot(pearson2, method = "color", col = palette, type = "upper",
+corrplot(pearson2_rename, method = "color", col = palette, type = "upper",
          order = "original", addCoef.col = "black", number.cex = 0.55, 
          number.digits = 2, tl.col = "black", tl.srt = 40, tl.cex = 0.8)
 dev.off()
 
 # save csv 
-write.csv(pearson2, paste0(csv_wd, "Reduced_Pearson_Correlation_Matrix.csv"))
+write.csv(pearson2_rename, paste0(csv_wd, "Correlation_Reduced_Predictor_Set.csv"))
 
 # explore correlation coefficients and VIFs
-vif2 = vif(env2, maxobservations = 10000)
+cl = snow::makeCluster(10)
+x2 = sampleRandom(env2, 10000, na.rm = TRUE, xy = TRUE)
+snow::stopCluster(cl)
+x2 = as.data.frame(x2) %>%
+  rename(Rugosity = rugosity,
+         BPI_Fine = bpi_fine,
+         Slope = slope) %>%
+  select(-x, -y)
+vif2 = vif(x2)
 vif2
-vifcor2 = vifcor(env2, th = abs(0.7), maxobservations = 10000)
-vifcor2
-write.csv(vif2, paste0(csv_wd, "Reduced_VIF.csv"), row.names = F)
+write.csv(vif2, paste0(csv_wd, "VIF_Reduced_Predictor_Set.csv"), row.names = F)
 
 #### ENM EVALUTATE ####
 #install.packages("rJava", dependencies = T)
@@ -148,7 +178,7 @@ system.file("java", package = "dismo")
 # Evaluate what the best settings are for MaxEnt 
 # need to read in the bias file created based on the distribution of survey effort, 
 # this will guide the selection of background points by MaxEnt
-bias = raster(paste0(train_wd, "Occurrences/bias.asc"))
+bias = raster(paste0(fish_wd, "Presence_Only/Bias.asc"))
 crs(bias) = my_crs
 
 # how many possible background points are available? 
@@ -158,19 +188,19 @@ length(which(!is.na(values(subset(env2, 1)))))
 bg = xyFromCell(bias, sample(which(!is.na(values(subset(env2, 1)))), 10000,
                              prob = values(bias)[!is.na(values(subset(env2, 1)))]))
 
-rm(env)
 
 # run evaluation using 10-fold cross-validation & background points selected based
 # on bias file. Remember to specify that variable 1 in the raster stack (habitat) 
 # is categorical and use only the presence data !
 enm_wd = "Z:/Courtney/Stuart_MSc_Ch1/ENMevaluate/"
 
-lg_pres_only = read.csv(paste0(train_wd, "Occurrences/Presence_Only/Subadult_Gray_Snapper_PO_Train.csv"))[,-1]
+lg_pres_only = read.csv(paste0(fish_wd, "Presence_Only/Subadult_Gray_Snapper_PO.csv"))[,-1]
 lg_enm_eval = ENMevaluate(lg_pres_only, env2, method = "randomkfold", kfolds = 10,
                        categoricals = 1, algorithm = 'maxent.jar', bg.coords = bg,
                        RMvalues = c(0.25, 0.50, 1.00, 1.50, 2.00, 5.00),
-                       fc = c("L", "LQ", "H", "LQH", "LQHP", "LQHPT"), parallel = TRUE, numCores = 15)
-write.csv(lg_enm_eval@results, paste(enm_wd, "lg_enmeval_results.csv"))
+                       fc = c("L", "LQ", "H", "LQH", "LQHP", "LQHPT"), 
+                       parallel = TRUE, numCores = 15)
+write.csv(lg_enm_eval@results, paste(enm_wd, "Subadult_Gray_Snapper_ENMeval.csv"))
 rm(lg_enm_eval)
 
 hs_pres_only = read.csv(paste0(train_wd, "Occurrences/Presence_Only/Subadult_Bluestriped_Grunt_PO_Train.csv"))[,-1]
